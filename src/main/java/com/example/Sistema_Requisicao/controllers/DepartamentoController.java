a8ecf97792022417f103a80217ea5b05c5c13d89
oXavier17 <ghenrique.xavier@hotmail.com>
2026-04-10 17:32:23 -0300

    atualizacao de correcao

--- src/main/java/com/example/Sistema_Requisicao/controllers/DepartamentoController.java
+++ src/main/java/com/example/Sistema_Requisicao/controllers/DepartamentoController.java
@@ -4,7 +4,6 @@ import com.example.Sistema_Requisicao.services.*;
 import com.example.Sistema_Requisicao.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
